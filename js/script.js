f53ff330f473decfc356d5a3cee297ca68461cda
supriya224 <52038704+supriya224@users.noreply.github.com>
2023-12-14 20:22:23 +0530

    commit all files

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
-const allFilterItems= document.querySelectorAll('.filter-items');
-const allFilterBtns= document.querySelectorAll('.filter-btn');
+filterObjects("all");
 
-allFilterBtns.forEach((btn)=>{
-    btn.addEventListener('click',()=>{
-        showFilterContent(btn);
-    })
-})
-function showFilterContent(btn){
-  allFilterItems.forEach((item)=>{
-    btn.classList.add('active-btn')
-    if(item.classList.contains(btn.id)){
-        item.style.display="block"
-    }
-  })
+function filterObjects(c) {
+  var x, i;
+  x = document.getElementsByClassName("collection-list");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    removeClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "show");
+  }
 }
 
-console.log(showFilterContent(btn))
+function addClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += " " + arr2[i];
+    }
+  }
+}
+
+function removeClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split("");
+  arr2 = name.split("");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join("");
+}
+// filterObjects("all");
